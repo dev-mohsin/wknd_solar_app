a4d241e29632e4728bff76d30e6b7bd306ffe003
Muhammad Mohsin <mohsin.flutter.dev@gmail.com>
2024-08-06 13:39:59 +0500

    Contacted and Appointment tab

--- lib/feature/home/presentation/pages/home_page.dart
+++ lib/feature/home/presentation/pages/home_page.dart
@@ -10,13 +10,14 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin {
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
   late TabController _tabController;
 
   @override
   void initState() {
     super.initState();
-    _tabController = TabController(length: 2, vsync: this);
+    _tabController = TabController(length: 4, vsync: this);
     context.read<ReferBloc>().add(FetchRefer());
   }
 
@@ -25,28 +26,44 @@ class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin
     return Scaffold(
       body: Column(
         children: [
-          const SizedBox(height: 16.0),
-          Text('Track your referral', style: context.titleLarge?.copyWith(fontSize: 24.0)),
-          const SizedBox(height: 16.0),
-          TabBar(
-            controller: _tabController,
-            tabs: const [
-              Tab(text: 'In Progress'),
-              Tab(text: 'Completed'),
+        
+            
+              const SizedBox(height: 16.0),
+              Text('Track your referral',
+                  style: context.titleLarge?.copyWith(fontSize: 24.0)),
+              const SizedBox(height: 16.0),
+                
+                 
+                    TabBar(
+                      isScrollable: true,
+                      controller: _tabController,
+                      tabs: const [
+                        Tab(text: 'Pending'),
+                        Tab(text: 'Contacted'),
+                        Tab(text: 'Appointment Set'),
+                        Tab(text: 'Completed'),
+                      ],
+                    ),
+                  
+                
+               
+              
+              Expanded(
+                child: TabBarView(
+                  controller: _tabController,
+                  children: [
+                    _ProgressCard(),
+                    _Contacted(),
+                    _AppointmentSet(),
+                    _CompleteTab(),
+                  ],
+                ),
+              ),
             ],
           ),
-          Expanded(
-            child: TabBarView(
-              controller: _tabController,
-              children: [
-                _ProgressCard(),
-                _CompleteTab(),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
+        
+      );
+    
   }
 }
 
@@ -57,16 +74,22 @@ class _ProgressCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocBuilder<ReferBloc, ReferState>(
       builder: (context, state) {
-        if (state is ReferLoading) return const Center(child: CircularProgressIndicator());
+        if (state is ReferLoading)
+          return const Center(child: CircularProgressIndicator());
         if (state is ReferFailure) return Center(child: Text(state.message));
-        if (state is ReferLoadedSuccess && state.refers.isEmpty) return const Center(child: Text('No refers found'));
+        if (state is ReferLoadedSuccess && state.refers.isEmpty)
+          return const Center(child: Text('No refers found'));
         if (state is ReferLoadedSuccess) {
-          final refers = state.refers.where((refer) => refer.status == 'In Progress').toList();
-          if (refers.isEmpty) return const Center(child: Text('No In Progress refers found'));
+          final refers = state.refers
+              .where((refer) => refer.status == 'In Progress')
+              .toList();
+          if (refers.isEmpty)
+            return const Center(child: Text('No In Progress refers found'));
           return ListView.separated(
             separatorBuilder: (context, index) => const SizedBox(height: 8.0),
             itemCount: refers.length,
-            padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 8.0),
+            padding:
+                const EdgeInsets.symmetric(horizontal: 12.0, vertical: 8.0),
             itemBuilder: (context, index) {
               final refer = refers[index];
               return Container(
@@ -84,7 +107,120 @@ class _ProgressCard extends StatelessWidget {
                       padding: const EdgeInsets.all(8.0),
                       child: Text(
                         refer.status,
-                        style: context.titleLarge?.copyWith(fontSize: 12.0, color: context.onPrimary),
+                        style: context.titleLarge?.copyWith(
+                            fontSize: 12.0, color: context.onPrimary),
+                      ),
+                    ),
+                  ),
+                ),
+              );
+            },
+          );
+        }
+        return const SizedBox.shrink();
+      },
+    );
+  }
+}
+
+class _Contacted extends StatelessWidget {
+  const _Contacted({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<ReferBloc, ReferState>(
+      builder: (context, state) {
+        if (state is ReferLoading)
+          return const Center(child: CircularProgressIndicator());
+        if (state is ReferFailure) return Center(child: Text(state.message));
+        if (state is ReferLoadedSuccess && state.refers.isEmpty)
+          return const Center(child: Text('No refers found'));
+        if (state is ReferLoadedSuccess) {
+          final refers = state.refers
+              .where((refer) => refer.status == 'Contacted')
+              .toList();
+          if (refers.isEmpty)
+            return const Center(child: Text('No Contacted Refers found'));
+          return ListView.separated(
+            separatorBuilder: (context, index) => const SizedBox(height: 8.0),
+            itemCount: refers.length,
+            padding:
+                const EdgeInsets.symmetric(horizontal: 12.0, vertical: 8.0),
+            itemBuilder: (context, index) {
+              final refer = refers[index];
+              return Container(
+                decoration: BoxDecoration(
+                  color: context.primary.withOpacity(0.1),
+                  borderRadius: BorderRadius.circular(16.0),
+                ),
+                child: ListTile(
+                  contentPadding: EdgeInsets.symmetric(horizontal: 8.0),
+                  title: Text(refer.theirName),
+                  subtitle: Text(refer.referralId),
+                  trailing: Card(
+                    color: context.tertiary,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        refer.status,
+                        style: context.titleLarge?.copyWith(
+                            fontSize: 12.0, color: context.onPrimary),
+                      ),
+                    ),
+                  ),
+                ),
+              );
+            },
+          );
+        }
+        return const SizedBox.shrink();
+      },
+    );
+  }
+}
+
+class _AppointmentSet extends StatelessWidget {
+  const _AppointmentSet({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<ReferBloc, ReferState>(
+      builder: (context, state) {
+        if (state is ReferLoading)
+          return const Center(child: CircularProgressIndicator());
+        if (state is ReferFailure) return Center(child: Text(state.message));
+        if (state is ReferLoadedSuccess && state.refers.isEmpty)
+          return const Center(child: Text('No refers found'));
+        if (state is ReferLoadedSuccess) {
+          final refers = state.refers
+              .where((refer) => refer.status == 'Appointment Set')
+              .toList();
+          if (refers.isEmpty)
+            return const Center(child: Text('No Appointment Refers found'));
+          return ListView.separated(
+            separatorBuilder: (context, index) => const SizedBox(height: 8.0),
+            itemCount: refers.length,
+            padding:
+                const EdgeInsets.symmetric(horizontal: 12.0, vertical: 8.0),
+            itemBuilder: (context, index) {
+              final refer = refers[index];
+              return Container(
+                decoration: BoxDecoration(
+                  color: context.primary.withOpacity(0.1),
+                  borderRadius: BorderRadius.circular(16.0),
+                ),
+                child: ListTile(
+                  contentPadding: EdgeInsets.symmetric(horizontal: 8.0),
+                  title: Text(refer.theirName),
+                  subtitle: Text(refer.referralId),
+                  trailing: Card(
+                    color: context.onTertiary,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        refer.status,
+                        style: context.titleLarge?.copyWith(
+                            fontSize: 12.0, color: context.onPrimary),
                       ),
                     ),
                   ),
@@ -106,16 +242,22 @@ class _CompleteTab extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocBuilder<ReferBloc, ReferState>(
       builder: (context, state) {
-        if (state is ReferLoading) return const Center(child: CircularProgressIndicator());
+        if (state is ReferLoading)
+          return const Center(child: CircularProgressIndicator());
         if (state is ReferFailure) return Center(child: Text(state.message));
-        if (state is ReferLoadedSuccess && state.refers.isEmpty) return const Center(child: Text('No refers found'));
+        if (state is ReferLoadedSuccess && state.refers.isEmpty)
+          return const Center(child: Text('No refers found'));
         if (state is ReferLoadedSuccess) {
-          final refers = state.refers.where((refer) => refer.status == 'Completed').toList();
-          if (refers.isEmpty) return const Center(child: Text('No Completed refers found'));
+          final refers = state.refers
+              .where((refer) => refer.status == 'Completed')
+              .toList();
+          if (refers.isEmpty)
+            return const Center(child: Text('No Completed refers found'));
           return ListView.separated(
             separatorBuilder: (context, index) => const SizedBox(height: 8.0),
             itemCount: refers.length,
-            padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 8.0),
+            padding:
+                const EdgeInsets.symmetric(horizontal: 12.0, vertical: 8.0),
             itemBuilder: (context, index) {
               final refer = refers[index];
               return Container(
@@ -125,7 +267,8 @@ class _CompleteTab extends StatelessWidget {
                 ),
                 child: ListTile(
                   contentPadding: EdgeInsets.symmetric(horizontal: 8.0),
-                  title: Text(refer.theirName, style: context.titleLarge?.copyWith(fontSize: 16.0)),
+                  title: Text(refer.theirName,
+                      style: context.titleLarge?.copyWith(fontSize: 16.0)),
                   subtitle: Text('Refer Id: ${refer.referralId}'),
                   trailing: Card(
                     color: Colors.green,
@@ -133,7 +276,8 @@ class _CompleteTab extends StatelessWidget {
                       padding: const EdgeInsets.all(8.0),
                       child: Text(
                         refer.status,
-                        style: context.titleLarge?.copyWith(fontSize: 12.0, color: context.onPrimary),
+                        style: context.titleLarge?.copyWith(
+                            fontSize: 12.0, color: context.onPrimary),
                       ),
                     ),
                   ),
